3e65e2ba222c96238d1bec0b63f0b1194ecb9325
Luis Martinez <mr.luiswebmaster@gmail.com>
2023-02-19 18:01:03 -0500

    testing deploy

--- controllers/index.js
+++ controllers/index.js
@@ -6,7 +6,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
   authStrategy: new LocalAuth(),
-  puppeteer:{ headless: false }
+  puppeteer:{ headless: false,args: ['--no-sandbox'], }
 });
 
 module.exports = {
